c93ef492ce36749ba38d896cf5b33a0cdd1d7f3c
Munyaradzi Makosa <munya@farmhutafrica.com>
2023-11-28 23:31:08 +0200

    Update about page layout and image width

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { Image } from "@nextui-org/image";
 
@@ -7,38 +8,34 @@ export default function AboutPage() {
 
   useEffect(() => {
     const handleResize = () => {
-      // Set image width to 400 if window width is greater than a certain breakpoint (e.g., 768px for tablets)
       if (window.innerWidth >= 768) {
-        setImageWidth(400);
+        setImageWidth(350);
       } else {
         setImageWidth(150);
       }
     };
 
-    // Listen for window resize events
     window.addEventListener('resize', handleResize);
-
-    // Set the initial size
     handleResize();
-
-    // Clean up listener
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-    <div className="flex flex-col md:flex-row justify-center md:justify-start items-center md:items-start gap-4 md:gap-8">
-      <div className="md:w-1/2 text-center md:text-left order-1 md:order-2">
-        <p className="text-[15px] md:text-lg md:w-max-lg">
-          We are a design studio that works at the intersection of everyday technology and art. Most of modern life is spent interacting with technology that is bland at best. Our objective is to bring beauty and simplicity to the mundane activities of life.
-        </p>
-      </div>
-      <div className="md:w-1/2 text-center justify-center items-center order-1 md:order-1">
+    <div className="flex flex-col md:flex-row justify-center md:justify-start items-center md:items-center gap-4 md:gap-40">
+      <div className="text-center justify-center items-center order-2 md:order-1 md:w-1/3">
         <Image
-          width={imageWidth} // Dynamic width based on device size
+          isZoomed
+          width={imageWidth}
           alt="NextUI hero Image"
           src="./IMG_2907.jpeg"
-          style={{ borderRadius: '0px' }} // Remove rounded corners
+          radius='none'
         />
+        <p className='text-[12px] mt-2'>Blue and Pink (fromXS)</p>
+      </div>
+      <div className="text-center md:text-left order-1 md:order-2 py-8 md:w-2/3">
+        <p className="text-[15px] md:text-lg md:max-w-3xl">
+          We are a design studio that works at the intersection of everyday technology and art. Most of modern life is spent interacting with technology that is bland at best. Our objective is to bring beauty and simplicity to the mundane activities of life.
+        </p>
       </div>
     </div>
   );
