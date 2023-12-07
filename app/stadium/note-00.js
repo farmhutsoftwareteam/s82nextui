a8735ac54429f0bc3fc78e88f509313f53c33708
Munyaradzi Makosa <munya@farmhutafrica.com>
2023-12-08 00:49:13 +0200

    fix text and shut

--- app/stadium/note-00.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Note00 = () => {
-  return (
-    <div>Note00</div>
-  )
-}
-
-export default Note00
--- app/stadium/note-01/page.js
+++ app/stadium/note-01/page.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
 
-const Note00 = () => {
+const Note01 = () => {
   return (
     <div className="container mx-auto p-4">
-      
+      <h1 className="text-2xl font-bold text-center  mb-6">Introduction</h1>
       <div className="flex flex-col md:flex-row items-center md:items-start">
         
         {/* Image container */}
@@ -20,12 +20,8 @@ const Note00 = () => {
         
         {/* Text container */}
         <div className="w-full md:w-1/2 md:pl-4 order-1 md:order-2">
-        <h1 className="text-2xl font-bold text-center md:text-left  mb-6">Introduction</h1>
           <p className="text-black items-center mt-4 md:mt-0">
             The game of soccer is something that I enjoy quite a lot. The beautiful play, the tactics, the gamesmanship, everything. These are all things I look forward to experiencing every time I get the chance to watch a game. Of course, there are times when this chance isn’t there and I have to rely on following the game online or through notifications. Experiencing the game in such a manner strips it of all its beauty. All I have left is the scoreline. I have no idea of who has been dominating the game, or whether the game has been clean, or any of the other information that makes soccer fun. This is what this project is about. Quickly telling the story of the game in as much detail as possible and doing so in a beautiful manner.
-          </p>
-          <p className="text-black items-center mt-4 md:mt-4">
-          Getting here was no easy feat though, and this is what this series of posts is all about. 
           </p>
         </div>
       </div>
@@ -33,4 +29,4 @@ const Note00 = () => {
   );
 };
 
-export default Note00;
+export default Note01;
