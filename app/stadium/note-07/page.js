d16fabc741886426ff159c39e50bc8ea6304a9f6
Munyaradzi Makosa <munya@farmhutafrica.com>
2023-12-18 22:00:39 +0200

    nn

--- app/stadium/note-07/page.js
+++ app/stadium/note-07/page.js
@@ -8,7 +8,7 @@ const Note = () => {
      
 
      
-      <h2 className='text-xl md:text-left text-center mb-6'>Problem Statement</h2>
+      <h2 className='text-xl md:text-left text-center mb-6'>Problem Statement </h2>
     
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div>
